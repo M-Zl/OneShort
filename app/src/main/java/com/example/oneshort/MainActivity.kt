1151a267990cdbe250437096270507d8386270e6
DESKTOP-15NP9FH\Three <ki3211@naver.com>
2025-02-26 17:06:16 +0900

    메인화면 test 생성

--- app/src/main/java/com/example/oneshort/MainActivity.kt
+++ app/src/main/java/com/example/oneshort/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.oneshort
 
+import android.content.Intent  // ← 여기에 추가!
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
+        // + 버튼 클릭 이벤트 추가
+        val fabAdd: FloatingActionButton = findViewById(R.id.fab_add)
+        fabAdd.setOnClickListener {
+            // 일정 추가 화면으로 이동
+            val intent = Intent(this, AddScheduleActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
